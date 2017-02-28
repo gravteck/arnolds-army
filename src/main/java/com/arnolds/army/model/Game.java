8c5984933da58edf2843bc6ab48f1344cef1ed6e
Matthew Heineke <matthew.heineke@gmail.com>
2017-02-28 09:04:43 -0500

    Moving away from modal functionality.

---
+++ src/main/java/com/arnolds/army/model/Game.java
@@ -0,0 +1,66 @@
+package com.arnolds.army.model;
+
+import java.time.LocalDateTime;
+
+public class Game {
+
+	private Integer id;
+
+	private LocalDateTime localDateTime;
+
+	private Team homeTeam;
+
+	private Team awayTeam;
+
+	private Integer homeScore;
+
+	private Integer awayScore;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
+
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+
+	public Team getHomeTeam() {
+		return homeTeam;
+	}
+
+	public void setHomeTeam(Team homeTeam) {
+		this.homeTeam = homeTeam;
+	}
+
+	public Team getAwayTeam() {
+		return awayTeam;
+	}
+
+	public void setAwayTeam(Team awayTeam) {
+		this.awayTeam = awayTeam;
+	}
+
+	public Integer getHomeScore() {
+		return homeScore;
+	}
+
+	public void setHomeScore(Integer homeScore) {
+		this.homeScore = homeScore;
+	}
+
+	public Integer getAwayScore() {
+		return awayScore;
+	}
+
+	public void setAwayScore(Integer awayScore) {
+		this.awayScore = awayScore;
+	}
+}
