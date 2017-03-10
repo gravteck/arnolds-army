6ebc7a5f23ba103bae0f9e385a0a6e189d9bef2c
Matthew Heineke <matthew.heineke@gmail.com>
2017-03-10 14:45:08 -0500

    Admin functional areas are now driven more generically through the templates and ReportingField instances.

--- src/main/java/com/arnolds/army/controller/AdminController.java
+++ src/main/java/com/arnolds/army/controller/AdminController.java
@@ -1,7 +1,20 @@
 package com.arnolds.army.controller;
 
+import static com.arnolds.army.model.ReportingField.delete;
+import static com.arnolds.army.model.ReportingField.edit;
+import static com.arnolds.army.model.ReportingField.link;
+import static com.arnolds.army.model.ReportingField.text;
+import static com.arnolds.army.model.ReportingField.view;
+import static com.arnolds.army.model.ReportingField.group;
+import static com.arnolds.army.model.ReportingField.reverseGroup;
+import static com.arnolds.army.model.ReportingField.spacer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +27,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.arnolds.army.FunctionalAreaType;
 import com.arnolds.army.model.Player;
+import com.arnolds.army.model.ReportingField;
+import com.arnolds.army.model.Team;
 import com.arnolds.army.service.ApplicationService;
 
 @Controller
@@ -23,6 +39,46 @@ public class AdminController {
 
 	@Autowired
 	private ApplicationService applicationService;
+
+	@GetMapping("{functionalArea}/list")
+	public String loadAdmin(Model m, @PathVariable String functionalArea) {
+
+		FunctionalAreaType functionalAreaType = FunctionalAreaType.find(functionalArea);
+		List<String> headers = new ArrayList<>();
+		List<List<ReportingField>> records = new ArrayList<>();
+		StringBuilder title = new StringBuilder();
+		StringBuilder itemFunctionalArea = new StringBuilder();
+
+		switch (functionalAreaType) {
+		case PLAYERS:
+			loadPlayers(headers, records, title, itemFunctionalArea);
+		case GAME:
+			break;
+		case GAMES:
+			break;
+		case PLAYER:
+			break;
+		case STATISTICAL_YEAR:
+			break;
+		case STATISTICAL_YEARS:
+			break;
+		case TEAM:
+			break;
+		case TEAMS:
+			loadTeams(headers, records, title, itemFunctionalArea);
+			break;
+		default:
+			break;
+		}
+
+		m.addAttribute("title", title);
+		m.addAttribute("addPath", "/admin/" + itemFunctionalArea + "/add/");
+		m.addAttribute("deletePath", "/admin/" + itemFunctionalArea + "/delete/");
+		m.addAttribute("headers", headers);
+		m.addAttribute("records", records);
+
+		return "admin/admin";
+	}
 
 	@GetMapping("players")
 	public String loadAdminPlayers(Model m) {
@@ -36,7 +92,24 @@ public class AdminController {
 
 		m.addAttribute("players", players);
 
-		return "admin/players-admin";
+		List<String> headers = new ArrayList<>(Arrays.asList("First Name", "Last Name", "Phone", "Email", "", "", ""));
+		// List<KeyValue> reportingFields = new
+		// ArrayList<>(ImmutableMap.of("firstName", ))
+
+		List<List<ReportingField>> records = players.stream()
+				.map(p -> Arrays.asList(text("firstName", p.getFirstName()), text("lastName", p.getLastName()),
+						link("phone", p.getPhone(), "tel:" + p.getPhone()),
+						link("email", p.getEmail(), "mailto:" + p.getEmail()), view("/player/" + p.getId()),
+						edit("/admin/player/edit/" + p.getId()), delete(p.getId())))
+				.collect(Collectors.toList());
+
+		m.addAttribute("title", "Players");
+		m.addAttribute("addPath", "/admin/player/add/");
+		m.addAttribute("deletePath", "/admin/player/delete/");
+		m.addAttribute("headers", headers);
+		m.addAttribute("records", records);
+
+		return "admin/players";
 	}
 
 	@GetMapping("player/edit/{playerId}")
@@ -78,16 +151,66 @@ public class AdminController {
 
 		redirectAttributes.addFlashAttribute("saved", Boolean.TRUE);
 
-		return "redirect:/admin/players";
+		return "redirect:/admin/players/list";
 	}
 
 	@RequestMapping("player/delete/{playerId}")
-	public String playerAddSubmit(@PathVariable Integer playerId, RedirectAttributes redirectAttributes) {
+	public String playerDeleteSubmit(@PathVariable Integer playerId, RedirectAttributes redirectAttributes) {
 
 		applicationService.removePlayer(playerId);
 
 		redirectAttributes.addFlashAttribute("deleted", Boolean.TRUE);
 
-		return "redirect:/admin/players";
+		return "redirect:/admin/players/list";
+	}
+
+	private void loadPlayers(List<String> headers, List<List<ReportingField>> records, StringBuilder title,
+			StringBuilder itemFunctionalArea) {
+		List<Player> players = applicationService.findAllPlayers();
+
+		title(title, FunctionalAreaType.PLAYERS.title());
+		itemFunctionalArea(itemFunctionalArea, FunctionalAreaType.PLAYER.value());
+		headers(headers, "First Name", "Last Name", "Phone", "Email", "");
+
+		records(records,
+				players.stream().map(p -> Arrays.asList(text("firstName", p.getFirstName()),
+						text("lastName", p.getLastName()), link("phone", p.getPhone(), "tel:" + p.getPhone()),
+						link("email", p.getEmail(), "mailto:" + p.getEmail()),
+						group(view("/" + itemFunctionalArea + "/" + p.getId()),
+								edit("/admin/" + itemFunctionalArea + "/edit/" + p.getId()), delete(p.getId()))))
+						.collect(Collectors.toList()));
+	}
+
+	private void loadTeams(List<String> headers, List<List<ReportingField>> records, StringBuilder title,
+			StringBuilder itemFunctionalArea) {
+		List<Team> teams = applicationService.findAllTeams();
+
+		title(title, FunctionalAreaType.TEAMS.title());
+		itemFunctionalArea(itemFunctionalArea, FunctionalAreaType.TEAM.value());
+		headers(headers, "Name", "", "");
+
+		records(records,
+				teams.stream().map(t -> Arrays.asList(text("Name", t.getName()), spacer(),
+						reverseGroup(view("/" + itemFunctionalArea + "/" + t.getId()),
+								edit("/admin/" + itemFunctionalArea + "/edit/" + t.getId()), delete(t.getId()))))
+						.collect(Collectors.toList()));
+	}
+
+	protected void title(StringBuilder title, String s) {
+		title.setLength(0);
+		title.append(s);
+	}
+
+	protected void itemFunctionalArea(StringBuilder itemFunctionalArea, String s) {
+		itemFunctionalArea.setLength(0);
+		itemFunctionalArea.append(s);
+	}
+
+	protected void headers(List<String> headers, String... s) {
+		headers.addAll(Arrays.asList(s));
+	}
+
+	protected void records(List<List<ReportingField>> records, Collection<List<ReportingField>> collection) {
+		records.addAll(collection);
 	}
 }
--- src/main/java/com/arnolds/army/model/BaseEntity.java
+++ src/main/java/com/arnolds/army/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package com.arnolds.army.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class)
 public class BaseEntity {
 
+	private List<ReportingField> reportingFields = new ArrayList<>();
+
+	public List<ReportingField> getReportingFields() {
+		return reportingFields;
+	}
 }
