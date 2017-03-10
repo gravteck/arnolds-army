04d5f75572a86b5a799c18bd4b2cf0a3d1d9b7e5
Matthew Heineke <matthew.heineke@gmail.com>
2017-03-10 14:45:44 -0500

    Missing files from previous commit.

---
+++ src/main/java/com/arnolds/army/FunctionalAreaType.java
@@ -0,0 +1,29 @@
+package com.arnolds.army;
+
+import java.util.Arrays;
+
+public enum FunctionalAreaType {
+	PLAYER("Player", "player"), PLAYERS("Players", "players"), TEAM("Team", "team"), TEAMS("Teams", "teams"), GAME(
+			"Game", "game"), GAMES("Games", "games"), STATISTICAL_YEAR("Statistical Year",
+					"statistical-year"), STATISTICAL_YEARS("Statistical Years", "statistical-years");
+
+	private String title;
+	private String value;
+
+	FunctionalAreaType(String title, String value) {
+		this.title = title;
+		this.value = value;
+	}
+
+	public String value() {
+		return this.value;
+	}
+
+	public String title() {
+		return this.title;
+	}
+
+	public static FunctionalAreaType find(String s) {
+		return Arrays.asList(FunctionalAreaType.values()).stream().filter(t -> t.value().equals(s)).findFirst().get();
+	}
+}
