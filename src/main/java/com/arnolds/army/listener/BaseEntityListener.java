450ee631232cc930b5c0340ea96acd210800e056
Matthew Heineke <matthew.heineke@gmail.com>
2017-03-15 14:51:32 -0400

    Base entities will now be created with insertion and update timestamps and their users from the Spring security context.

---
+++ src/main/java/com/arnolds/army/listener/BaseEntityListener.java
@@ -0,0 +1,30 @@
+package com.arnolds.army.listener;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.arnolds.army.model.BaseEntity;
+
+public class BaseEntityListener {
+
+	@PrePersist
+	protected void onCreate(BaseEntity be) {
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		be.setInsertUser(name);
+		be.setInsertDate(LocalDateTime.now());
+	}
+
+	@PreUpdate
+	protected void onUpdate(BaseEntity be) {
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		be.setUpdateUser(name);
+		be.setUpdateDate(LocalDateTime.now());
+	}
+}
