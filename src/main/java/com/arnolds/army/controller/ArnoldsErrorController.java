c977c596b36f0760f42d9dc9d94af2b2705827ad
Matthew Heineke <matthew.heineke@gmail.com>
2017-04-17 10:15:01 -0400

    Created catch-all error page. Admin users that are logged in will see a stack trace for the given error.

---
+++ src/main/java/com/arnolds/army/controller/ArnoldsErrorController.java
@@ -0,0 +1,22 @@
+package com.arnolds.army.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ArnoldsErrorController implements ErrorController {
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+	@RequestMapping("error")
+	public String handleError(Model m, Exception e) {
+
+		m.addAttribute("stackTrace", e.getStackTrace());
+		return "error";
+	}
+}
